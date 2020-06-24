6f1cccd03c39618255501af7c1dfb52d16d1ef2f
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-06-23 23:51:19 -0300

    Implement total cart on FloatingCart

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,9 +24,7 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    return products.reduce((total, product) => total + (product.price * product.quantity), 0);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
@@ -44,7 +42,7 @@ const FloatingCart: React.FC = () => {
       </CartButton>
 
       <CartPricing>
-        <CartTotalPrice>{cartTotal}</CartTotalPrice>
+        <CartTotalPrice>{formatValue(cartTotal)}</CartTotalPrice>
       </CartPricing>
     </Container>
   );
